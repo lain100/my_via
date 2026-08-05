0c622760c3cee9c56761321ba6301b3c30274182
lain100 <lain@Lain.localdomain>
2026-08-05 20:22:42 +0900

    fourteenth

--- keymap.c
+++ keymap.c
@@ -93,7 +93,9 @@ typedef struct {
     bool     registered;
 } morph_key_t;
 
-const uint16_t del_rep_delay[33] = {400, 99, 79, 65, 57, 49, 43, 40, 35, 33, 30, 28, 26, 25, 23, 22, 20, 20, 19, 18, 17, 16, 15, 15, 14, 14, 13, 13, 12, 12, 11, 11, 10};
+const uint16_t del_rep_delay[33] = {
+    400, 99, 79, 65, 57, 49, 43, 40, 35, 33, 30, 28, 26, 25, 23, 22, 20, 20, 19, 18, 17, 16, 15, 15, 14, 14, 13, 13, 12, 12, 11, 11, 10,
+};
 
 static morph_key_t nav, del;
 
@@ -111,9 +113,11 @@ void repeat_keys(void) {
 }
 
 void within_word(uint16_t keycode) {
-    static const uint16_t brcts[][2]   = {{S(KC_QUOT), S(KC_QUOT)}, {S(KC_LBRC), S(KC_RBRC)}, {S(KC_COMM), S(KC_DOT)}, {S(KC_9), S(KC_0)}, {KC_QUOT, KC_QUOT}, {KC_LBRC, KC_RBRC}, {KC_GRV, KC_GRV}, {0, -1}};
-    static const uint8_t  null_id      = ARRAY_SIZE(brcts) - 1;
-    static uint8_t        reception_id = null_id;
+    static const uint16_t brcts[][2] = {
+        {S(KC_QUOT), S(KC_QUOT)}, {S(KC_LBRC), S(KC_RBRC)}, {S(KC_COMM), S(KC_DOT)}, {S(KC_9), S(KC_0)}, {KC_QUOT, KC_QUOT}, {KC_LBRC, KC_RBRC}, {KC_GRV, KC_GRV}, {0, -1},
+    };
+    static const uint8_t null_id      = ARRAY_SIZE(brcts) - 1;
+    static uint8_t       reception_id = null_id;
     keycode &= 0xFF;
 
     if (is_caps_word_on()) {
@@ -214,6 +218,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_on(4);
     }
 
+    static bool is_swap_hands_toggle;
+    if (keycode == LT(0, 1)) {
+        if (record->event.pressed) {
+            if (record->tap.count == 1) {
+                if (is_swap_hands_toggle) {
+                    is_swap_hands_toggle = false;
+                    swap_hands_off();
+                } else {
+                    swap_hands_on();
+                }
+            } else if (record->tap.count) {
+                is_swap_hands_toggle = true;
+                swap_hands_on();
+            } else {
+                is_swap_hands_toggle = false;
+                swap_hands_off();
+                nav.type = NAV_Tab;
+            }
+        }
+        return false;
+    } else if (!is_swap_hands_toggle) {
+        swap_hands_off();
+    }
+
     switch (keycode) {
         case LT(0, KC_3):
         case LT(0, KC_X):
@@ -283,9 +311,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
         case LT(0, KC_LNG1):
         case LT(0, KC_LNG2):
-            if (is_swap_hands_on()) {
-                swap_hands_off();
-            }
             if (record->event.pressed) {
                 tap_code(keycode == LT(0, KC_LNG2) ? KC_F13 : KC_F14);
                 if (record->tap.count) {
@@ -337,16 +362,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
         case KC_BSPC:
+        case KC_DEL:
             if (record->event.pressed) {
-                const uint8_t saved_mods = get_mods();
-                if ((saved_mods | get_oneshot_mods()) & MOD_LSFT) {
-                    keycode = KC_DEL;
-                    del_oneshot_mods(MOD_LSFT);
-                    unregister_mods(MOD_LSFT);
-                }
                 tap_code(keycode);
-                set_mods(saved_mods);
-                del = (morph_key_t){.keycode = keycode, .timer = timer_read(), .registered = true};
+                del = (morph_key_t){
+                    .keycode    = keycode,
+                    .timer      = timer_read(),
+                    .registered = true,
+                };
             } else {
                 del.registered = false;
             }
@@ -380,27 +403,45 @@ void matrix_scan_user(void) {
     repeat_keys();
 }
 
-enum combos { CMB_INT4, CMB_SH_OS, CMB_SH_TOG1, CMB_SH_TOG2, CMB_SH_OFF, CMB_LNG1, CMB_LNG2, CMB_PSCR, CMB_OS_CTL, CMB_OS_SFT, CMB_OS_ALT, CMB_OS_GUI, CMB_MS_BTN1, CMB_MS_BTN2, CMB_MS_BTN3 };
+enum combos {
+    CMB_INT4,
+    CMB_SH_OS_TOGG1,
+    CMB_DEL1,
+    CMB_DEL2,
+    CMB_SH_OS_TOGG2,
+    CMB_LNG1,
+    CMB_LNG2,
+    CMB_PSCR,
+    CMB_OS_CTL,
+    CMB_OS_SFT,
+    CMB_OS_ALT,
+    CMB_OS_GUI,
+    CMB_MS_BTN1,
+    CMB_MS_BTN2,
+    CMB_MS_BTN3,
+};
 
 #define RCA_T(k) (MT(MOD_RCTL | MOD_RALT, (k)))
 
-const uint16_t PROGMEM cmb_int4[]    = {KC_Z, KC_K, COMBO_END};
-const uint16_t PROGMEM cmb_sh_os[]   = {KC_M, KC_K, COMBO_END};
-const uint16_t PROGMEM cmb_sh_tog1[] = {KC_Z, KC_M, COMBO_END};
-const uint16_t PROGMEM cmb_sh_tog2[] = {KC_DOT, KC_MINS, COMBO_END};
-const uint16_t PROGMEM cmb_sh_off[]  = {RCA_T(KC_C), KC_DOT, COMBO_END};
-const uint16_t PROGMEM cmb_lng1[]    = {LCTL_T(KC_S), KC_G, COMBO_END};
-const uint16_t PROGMEM cmb_lng2[]    = {LT(0, KC_X), RCTL_T(KC_Y), COMBO_END};
-const uint16_t PROGMEM cmb_pscr[]    = {KC_L, KC_D, KC_W, COMBO_END};
-const uint16_t PROGMEM cmb_os_ctl[]  = {KC_D, KC_W, COMBO_END};
-const uint16_t PROGMEM cmb_os_sft[]  = {KC_L, KC_W, COMBO_END};
-const uint16_t PROGMEM cmb_os_alt[]  = {KC_L, KC_D, COMBO_END};
-const uint16_t PROGMEM cmb_os_gui[]  = {KC_Q, KC_L, COMBO_END};
-const uint16_t PROGMEM cmb_ms_btn1[] = {LSFT_T(KC_T), LCTL_T(KC_S), COMBO_END};
-const uint16_t PROGMEM cmb_ms_btn2[] = {LALT_T(KC_R), LSFT_T(KC_T), COMBO_END};
-const uint16_t PROGMEM cmb_ms_btn3[] = {LALT_T(KC_R), LCTL_T(KC_S), COMBO_END};
+const uint16_t PROGMEM cmb_int4[]        = {KC_Z, KC_K, COMBO_END};
+const uint16_t PROGMEM cmb_sh_os_togg1[] = {KC_M, KC_K, COMBO_END};
+const uint16_t PROGMEM cmb_del1[]        = {KC_Z, KC_M, COMBO_END};
+const uint16_t PROGMEM cmb_del2[]        = {KC_DOT, KC_MINS, COMBO_END};
+const uint16_t PROGMEM cmb_sh_os_togg2[] = {RCA_T(KC_C), KC_DOT, COMBO_END};
+const uint16_t PROGMEM cmb_lng1[]        = {LCTL_T(KC_S), KC_G, COMBO_END};
+const uint16_t PROGMEM cmb_lng2[]        = {LT(0, KC_X), RCTL_T(KC_Y), COMBO_END};
+const uint16_t PROGMEM cmb_pscr[]        = {KC_L, KC_D, KC_W, COMBO_END};
+const uint16_t PROGMEM cmb_os_ctl[]      = {KC_D, KC_W, COMBO_END};
+const uint16_t PROGMEM cmb_os_sft[]      = {KC_L, KC_W, COMBO_END};
+const uint16_t PROGMEM cmb_os_alt[]      = {KC_L, KC_D, COMBO_END};
+const uint16_t PROGMEM cmb_os_gui[]      = {KC_Q, KC_L, COMBO_END};
+const uint16_t PROGMEM cmb_ms_btn1[]     = {LSFT_T(KC_T), LCTL_T(KC_S), COMBO_END};
+const uint16_t PROGMEM cmb_ms_btn2[]     = {LALT_T(KC_R), LSFT_T(KC_T), COMBO_END};
+const uint16_t PROGMEM cmb_ms_btn3[]     = {LALT_T(KC_R), LCTL_T(KC_S), COMBO_END};
 
-combo_t key_combos[] = {[CMB_INT4] = COMBO(cmb_int4, KC_INT4), [CMB_SH_OS] = COMBO(cmb_sh_os, SH_OS), [CMB_SH_TOG1] = COMBO(cmb_sh_tog1, SH_TOGG), [CMB_SH_TOG2] = COMBO(cmb_sh_tog2, SH_TOGG), [CMB_SH_OFF] = COMBO(cmb_sh_off, SH_OFF), [CMB_LNG1] = COMBO(cmb_lng1, LT(0, KC_LNG1)), [CMB_LNG2] = COMBO(cmb_lng2, LT(0, KC_LNG2)), [CMB_PSCR] = COMBO(cmb_pscr, KC_PSCR), [CMB_OS_CTL] = COMBO(cmb_os_ctl, OSM(MOD_LCTL)), [CMB_OS_SFT] = COMBO(cmb_os_sft, OSM(MOD_LSFT)), [CMB_OS_ALT] = COMBO(cmb_os_alt, OSM(MOD_LALT)), [CMB_OS_GUI] = COMBO(cmb_os_gui, OSM(MOD_LGUI)), [CMB_MS_BTN1] = COMBO(cmb_ms_btn1, KC_MS_BTN1), [CMB_MS_BTN2] = COMBO(cmb_ms_btn2, KC_MS_BTN2), [CMB_MS_BTN3] = COMBO(cmb_ms_btn3, KC_MS_BTN3)};
+combo_t key_combos[] = {
+    [CMB_INT4] = COMBO(cmb_int4, KC_INT4), [CMB_SH_OS_TOGG1] = COMBO(cmb_sh_os_togg1, LT(0, 1)), [CMB_DEL1] = COMBO(cmb_del1, KC_DEL), [CMB_DEL2] = COMBO(cmb_del2, KC_DEL), [CMB_SH_OS_TOGG2] = COMBO(cmb_sh_os_togg2, LT(0, 1)), [CMB_LNG1] = COMBO(cmb_lng1, LT(0, KC_LNG1)), [CMB_LNG2] = COMBO(cmb_lng2, LT(0, KC_LNG2)), [CMB_PSCR] = COMBO(cmb_pscr, KC_PSCR), [CMB_OS_CTL] = COMBO(cmb_os_ctl, OSM(MOD_LCTL)), [CMB_OS_SFT] = COMBO(cmb_os_sft, OSM(MOD_LSFT)), [CMB_OS_ALT] = COMBO(cmb_os_alt, OSM(MOD_LALT)), [CMB_OS_GUI] = COMBO(cmb_os_gui, OSM(MOD_LGUI)), [CMB_MS_BTN1] = COMBO(cmb_ms_btn1, KC_MS_BTN1), [CMB_MS_BTN2] = COMBO(cmb_ms_btn2, KC_MS_BTN2), [CMB_MS_BTN3] = COMBO(cmb_ms_btn3, KC_MS_BTN3),
+};
 
 bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode, keyrecord_t *record) {
     switch (combo_index) {
@@ -445,6 +486,9 @@ bool caps_word_press_user(uint16_t keycode) {
 report_mouse_t pointing_device_task_kb(report_mouse_t mouse_report) {
     if (del.registered) {
         const uint16_t keycode = mouse_report.x > VOL_TENSION_THRESHOLD ? KC_VOLU : mouse_report.x < -VOL_TENSION_THRESHOLD ? KC_VOLD : 0;
+        if (keycode) {
+            del.keycode = 0;
+        }
         register_code(keycode);
         unregister_code(keycode);
         mouse_report = (report_mouse_t){};
