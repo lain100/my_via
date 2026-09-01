66047a00179db128f3753eebd5a7df876b1613cc
lain100 <lain@Lain.localdomain>
2026-09-01 20:14:46 +0900

    thirty third

--- keymap.c
+++ keymap.c
@@ -372,7 +372,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case LT(3, KC_ENT):
             if (is_alternative_swap_hands && record->tap.count) {
                 if (record->event.pressed) {
-                    tap_code(keycode == LT(4, KC_SPC) ? KC_H : KC_P);
+                    tap_code(keycode == LT(4, KC_SPC) ? KC_H : KC_F);
                 }
                 return false;
             }
@@ -418,7 +418,7 @@ const uint16_t PROGMEM cmb_pscr[]        = {LAG_T(KC_L), LSG_T(KC_D), LCG_T(KC_W
 const uint16_t PROGMEM cmb_os_ctl[]      = {LCSG_T(KC_D), LCG_T(KC_W), COMBO_END};
 const uint16_t PROGMEM cmb_os_sft[]      = {LAG_T(KC_L), LCG_T(KC_W), COMBO_END};
 const uint16_t PROGMEM cmb_os_alt[]      = {LAG_T(KC_L), LCSG_T(KC_D), COMBO_END};
-const uint16_t PROGMEM cmb_os_gui[]      = {KC_Q, LAG_T(KC_L), COMBO_END};
+const uint16_t PROGMEM cmb_os_gui[]      = {KC_P, LAG_T(KC_L), COMBO_END};
 const uint16_t PROGMEM cmb_ms_btn1[]     = {LSFT_T(KC_T), LCTL_T(KC_S), COMBO_END};
 const uint16_t PROGMEM cmb_ms_btn2[]     = {LALT_T(KC_R), LSFT_T(KC_T), COMBO_END};
 const uint16_t PROGMEM cmb_ms_btn3[]     = {LALT_T(KC_R), LCTL_T(KC_S), COMBO_END};
@@ -483,11 +483,14 @@ __attribute__((weak)) const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRI
 };
 #endif
 
+#define RCG_T(k) (MT(MOD_RCTL | MOD_RGUI, (k)))
+#define RCSG_T(k) (MT(MOD_RCTL | MOD_RSFT | MOD_RGUI, (k)))
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [0] = LAYOUT_universal(
-    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
+    KC_P     , LAG_T(KC_L),LCSG_T(KC_D),LCG_T(KC_W) , LT(0, KC_NO),                            LT(0, KC_NO), RCG_T(KC_Q),RCSG_T(KC_O), RAG_T(KC_U) , KC_BSPC,
     KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
     KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
     KC_LCTL  , KC_LGUI  , KC_LALT  ,LSFT_T(KC_LNG2),LT(1,KC_SPC),LT(3,KC_LNG1),KC_BSPC,LT(2,KC_ENT),LSFT_T( KC_LNG2),KC_RALT,KC_RGUI, KC_RSFT),
